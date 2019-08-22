fa4d0aa5ef91b1a8a419388f5a965cc99da3d165
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-08-22 11:44:29 +0300

    Update model_selection.R

--- model_selection.R
+++ model_selection.R
@@ -9,10 +9,19 @@ a <- read.table("FOR_ABC_CLEAN", h=F)
 cross_val_data=sample_n(a,1000)
 a=anti_join(a,cross_val_data)
 
-
+#build training dataset
 modindex= a[,1]  #indexes of model
+modindex=sapply(modindex,as.factor)
 sumsta= a[,-(1:14)] #summary stats
-data1 = data.frame(modindex, sumsta) #combined?
+data1 = data.frame(modindex, sumsta) #combined
 
 
-model.rf1 <- abcrf(modindex~., data = data1,ncores=8, ntree=100,lda=FALSE) #our model, does not run with lda=TRUE
+model.rf1 <- abcrf(modindex~., data = data1,ncores=16, ntree=10000,lda=FALSE) #our model, does not run with lda=TRUE
+
+
+sumstb= cross_val_data[,-(1:14)]
+truindex= cross_val_data[,1]
+truindex=sapply(truindex,as.factor)
+err.rf <- err.abcrf(model.rf1, data1)
+
+model_predict=predict(model.rf1,sums
