435a05ea96eb168dbc28e66a881899caa9d50e6f
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-08-30 11:18:00 +0300

    Update ambracia_simulations_reworked.py

--- ambracia_simulations_reworked.py
+++ ambracia_simulations_reworked.py
@@ -224,11 +224,11 @@ for REPS in range(0,reps):
 
      
 ################################################################ RUN COMUSTATS #################################################################
-
+    
     os.system('CoMuStats -input CHUNKED_{}  -npop 3 20 20 20 -pairwiseFst -ms -sepPops > COMUSTATS_{}'.format(REPS,REPS))
     elapsed_time_4=time.time() - start_time
     print('step 3 : {}'.format(elapsed_time_4/60)) 
-        
+    os.system('rm CHUNKED_*')    
 ###############################################################################################################################################
 
 
