0a7c87c4c661f628a67107ad6564d90e02f5f8e4
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-09-24 10:49:46 +0300

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -6,12 +6,13 @@ Created on Wed Feb 14 14:58:03 2018
 """
 
 import math
+import random
 import numpy as np
 from numpy import genfromtxt
 from numpy import random
 import timeit
 import matplotlib.pyplot as plt
-#import pandas as pd #not sure if needed
+import pandas as pd 
 #model selection
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -28,6 +29,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.externals import joblib
+
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
@@ -50,6 +52,7 @@ from sklearn.metrics import roc_curve
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectFromModel
 from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import VarianceThreshold
 
 #how to crash a server 101
 from sklearn.externals import joblib
@@ -77,27 +80,43 @@ X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=0.2, random_st
     
 print(len(LABELS),len(DATA))
 
+randints=np.random.randint(0,len(DATA),size=1000,dtype='int')
+print(randints)
+
+
+Final_data=[DATA[x] for x in randints]
+Final_labels=[LABELS[x] for x in randints]
+
+DATA=[DATA[x] for x in range(0,len(DATA)) if x not in randints]
+LABELS=[LABELS[x] for x in range(0,len(LABELS)) if x not in randints]
+
+print(len(Final_data),len(Final_labels))
+print(len(DATA),len(LABELS))
+
 
 
 ###-------------------------- Random Forests --------------------------
 print("Random Forest")
 
 
-pipeline_rf = make_pipeline(StandardScaler(), RandomForestClassifier( n_jobs=10))
+pipeline_rf = make_pipeline(StandardScaler(),VarianceThreshold(threshold=(.8 * (1 - .8))), RandomForestClassifier( n_jobs=16))
 
 crit = ['gini', 'entropy']
-numt = [100, 150] #number of trees
+numt = [100, 150,300,500,1000] #number of trees
 feats = ['log2','sqrt'] #max_features considered during each split
-depth = [ 20, 30, None] #maximum depth each tree is allowed to reach
+depth = [ 20, 30,40,50,None] #maximum depth each tree is allowed to reach
+#variances=[.8*(1 - .8),]
+
+
 # 
 hyperparameters = {#'randomforestclassifier__criterion' : crit,
                    'randomforestclassifier__n_estimators' : numt,
                    'randomforestclassifier__max_features' : feats,
+                   #'VarianceThreshold__max_features' : feats,
                    'randomforestclassifier__max_depth': depth}
 
 
-nested_score = np.zeros([60, 1]).astype(int)
-print(nested_score.shape)
+
 
 inner_cv = KFold(n_splits=10, shuffle=True)
 outer_cv = KFold(n_splits=5, shuffle=True)
@@ -105,65 +124,50 @@ outer_cv = KFold(n_splits=5, shuffle=True)
 ## Pass the gridSearch estimator to cross_val_score
 clf = GridSearchCV(pipeline_rf, param_grid=hyperparameters, cv=inner_cv)
 
-set_scores = []
+clf.fit(DATA,LABELS)
 
 #for each demographic pair we perform nested cross validation
 
-for i in range(1, 61):
-    kappa = cross_val_score(clf, X=DATA, y=LABELS, cv=outer_cv, n_jobs=10).mean()
-    nested_score[i-1,0] = kappa
-    set_scores.append(kappa)
-    print("dataset" + str(i))
-with open ("forest_res.txt",'w') as rf:
-    for j in range (0, 60):
-        rf.write(str(set_scores[j]))
-    
+print(pd.DataFrame.from_dict(clf.cv_results_))
+print('#######################################')
+print(clf.best_estimator_)
+print(clf.best_params_)
+
+print('#######################################')
+
+predictedd=clf.predict(Final_data)
+results=open('results.txt.','w')
+print(predictedd)
+print(Final_labels)
+Predictions=[]
+for k in range(0,len(predictedd)):
+    results.write(str(predictedd[k]))
+    results.write('\t')
+    results.write(str(Final_labels[k]))
+    results.write('\n')
+    if str(predictedd[k])==str(Final_labels[k]):
+        Predictions.append(1)
+    else:
+        Predictions.append(0)
+
+print(np.mean(Predictions))
+results.write(str(mean(Predictions)))
+#print(clf.cv_results_[clf.best_index_])
+kappa = cross_val_score(clf, X=DATA, y=LABELS, cv=outer_cv, n_jobs=12)
+
+
 print(kappa)
 
-print(set_scores)
-
+#print(set_scores)
+print('finished')
 #############################################################################################################################################################
-#############################################################################################################################################################
-
-nested_scores = {}
-
-clf_names = ['Corinthian Origin', 'Local Origin']
-
-true_pos = []
-false_pos = []
-
-accuracies = np.zeros((61,3)).astype(float)
-print(accuracies.shape)
-
-dtst = 0
-def get_the_rep(y_true, y_pred):
-    global dtst
-    nested_scores.update({'params'+str(dtst) : classification_report(y_pred,y_true,target_names = clf_names)})
-    
-    fpr, tpr, _ = roc_curve(y_true, y_pred, pos_label=0)
-    true_pos.append(tpr)
-    false_pos.append(fpr)         
-    
-    
-    dtst = dtst + 1
-    return accuracy_score(y_true, y_pred)
-
-
-#def get_the_rep(y_true, y_pred):
-#    global dtst
-#    best_params.update({'params'+str(dtst) : classification_report(y_pred,y_true,target_names = clf_names)})
-#    dtst = dtst + 1
-#    return accuracy_score(y_true, y_pred)
-
-
-
-#############################################################################################################################################################
+##################################################################################################################################################
 #############################################################################################################################################################
 #############################################################################################################################################################
 
 #------------------ Support Vector Machines ---------------
 print("Support Vector Machines")
-pipeline_svm = make_pipeline(StandardScaler(), SVC())
+pipeline_svm = make_pipeline(StandardScaler(),SVC())
 cst = [1, 2, 5, 7, 10] #starting from hard-margin and loosening to see performance
 ker = ['poly']
 deg = [1] #just for the polynomial kernel
@@ -177,36 +181,22 @@ hyperparameters = { 'svc__C' : cst,
 inner_cv = KFold(n_splits=10, shuffle=True)
 outer_cv = KFold(n_splits=5, shuffle=True)
 
+print('CV')
+print(outer_cv)
+
 ## Pass the gridSearch estimator to cross_val_score
-clf = GridSearchCV(pipeline_svm, param_grid=hyperparameters, cv=inner_cv, n_jobs=-1)
+clf = GridSearchCV(pipeline_svm, param_grid=hyperparameters, cv=inner_cv, n_jobs=12)
 
 
+set_scores_svm=[]
+FEATS=[30,50,90]
 
-#for each demographic pair we perform nested cross validation
-for feats in range (40, 41):
-    
-    set_scores_svm = []
- #   global true_pos, false_pos, nested_scores
+#   global true_pos, false_pos, nested_scores
+for feats in FEATS:
+    selected = SelectKBest(score_func=mutual_info_classif, k=feats).fit(DATA, LABELS)
+    current = selected.transform(DATA)
 
-    nested_scores = {}
-    true_pos = []
-    false_pos = []
-    
-    print (feats)
-    for i in range(1, num_sets):
-        print(i)
-        data = np.concatenate((neut_data['neut'+str(i)],slct_data['sel'+str(i)]), axis = 0)
-        selected = SelectKBest(score_func=mutual_info_classif, k=feats).fit(data, labels[:,0])
-        current = selected.transform(data)
-        
-        kappa = cross_val_score(clf, X=current, y=labels[:,0], cv=outer_cv, scoring = make_scorer(get_the_rep)).mean()
-        set_scores_svm.append(kappa)    
-    
-    with open ("split_res/acc/tot_acc_split" + str(feats) + ".txt", "w") as rf:
-        for j in range (0, 60):
-           rf.write(str(set_scores_svm[j]) + "\n")
-    
-    np.savetxt("split_res/tpr/tpr_split" + str(feats) + ".txt", np.array(true_pos[:]),  fmt = '%1.8f')
-    np.savetxt("split_res/fpr/fpr_split" + str(feats) + ".txt",  np.array(false_pos[:]),  fmt = '%1.8f' )
-    #np.savetxt("split_res/nested/rep_split" + str(feats) + ".txt", np.array(true_pos[:]),  fmt = '%1.8f')
-    	#rp.write(str(nested_scores))
+    kappa = cross_val_score(clf, X=current, y=LABELS, cv=outer_cv).mean()
+    set_scores_svm.append(kappa)
+    print(kappa)
+print(set_scores_svm)
