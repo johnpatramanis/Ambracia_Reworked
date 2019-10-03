2cd34bebca2a2ab0dbb3831d9e897b492105d3ac
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-10-03 16:02:17 +0300

    Add files via upload

---
+++ ABC_plots.r
@@ -0,0 +1,37 @@
+mydirectory=getwd()
+setwd(mydirectory)
+
+library(abc)
+library(Metrics)
+library(dplyr)
+k=5
+library(RColorBrewer); my.cols <- rev(brewer.pal(k, "RdYlBu")) 
+
+
+a <- read.table("FOR_SVM", h=F)
+dim(a)
+
+LOGIT_MATRIX=matrix(nrow=13,ncol=2)
+for (Z in 1:13){
+  
+  LOGIT_MATRIX[Z,1]=min(a[,Z+1])
+  LOGIT_MATRIX[Z,2]=max(a[,Z+1])
+  
+}
+
+
+cross_val_data=sample_n(a,1000)
+a=anti_join(a,cross_val_data)
+
+
+params <- a[,2:14]   
+stats <- a[,-(1:14)]
+test <- cross_val_data[1,-(1:14)]
+test_params <-cross_val_data[1,2:14]
+
+
+
+myabc <- abc(target=test, param=params, sumstat=stats, tol=0.1, method="ridge", hcorr=TRUE,transf=c('logit'),logit.bounds=LOGIT_MATRIX)
+summarystats=summary(myabc)
+
+
