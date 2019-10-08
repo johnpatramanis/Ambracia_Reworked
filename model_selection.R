618ab97ca8b5e44754ed7665f6392a9b4013a6c2
Ioannis Patramanis <32954442+johnpatramanis@users.noreply.github.com>
2019-10-08 15:34:15 +0300

    Add files via upload

--- model_selection.R
+++ model_selection.R
@@ -4,9 +4,9 @@ setwd(mydir)
 library(dplyr)
 library(abcrf)
 
-a <- read.table("FOR_ABC_CLEAN", h=F)
+a <- read.table("FOR_SVM", h=F)
 
-cross_val_data=sample_n(a,10000)
+cross_val_data=sample_n(a,1000)
 a=anti_join(a,cross_val_data)
 
 #build training dataset
@@ -22,6 +22,31 @@ model.rf1 <- abcrf(modindex~., data = data1,ncores=16, ntree=10000,lda=FALSE) #o
 sumstb= cross_val_data[,-(1:14)]
 truindex= cross_val_data[,1]
 truindex=sapply(truindex,as.factor)
+
 err.rf <- err.abcrf(model.rf1, data1)
+print(err.rf)
+
+
+
+model.rf.r <- regAbcrf(modindex~., data1,ncores=4, ntree=10000)
+print(model.rf.r)
+
+
+
+myerror=err.regAbcrf(model.rf.r, data1)
+print(myerror)
+
+
 
 model_predict=predict(model.rf1,sumstb,data1,ntree=100)
+print(model_predict)
+
+
+
+model_predict=predict(model.rf1,sumstb,data1,ntree=1000)
+print(model_predict)
+
+
+model_predict=predict(model.rf1,sumstb,data1,ntree=10000)
+print(model_predict)
+
